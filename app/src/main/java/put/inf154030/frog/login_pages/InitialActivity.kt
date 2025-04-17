1195c9437e0cf4d8a18e17661fbb1944fb79ec45
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-04-17 16:48:30 +0200

    organized imports and deleted preview

--- app/src/main/java/put/inf154030/frog/login_pages/InitialActivity.kt
+++ app/src/main/java/put/inf154030/frog/login_pages/InitialActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,11 +12,9 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import put.inf154030.frog.R
 import put.inf154030.frog.theme.FrogTheme
@@ -62,49 +59,6 @@ class InitialActivity : ComponentActivity() {
                             )
                         }
                     }
-                }
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    FrogTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.logo),
-                    contentDescription = "App logo",
-                    modifier = Modifier
-                        .fillMaxSize(0.75f)
-                )
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .fillMaxWidth(0.5f),
-                ) {
-                    Text(
-                        text = "Log In"
-                    )
-                }
-                Button(
-                    onClick = {},
-                    modifier = Modifier
-                        .padding(vertical = 8.dp)
-                        .fillMaxWidth(0.5f),
-                ) {
-                    Text(
-                        text = "Sign Up"
-                    )
                 }
             }
         }
