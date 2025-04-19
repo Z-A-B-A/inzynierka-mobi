e803c353647c2695a5e70275075cdbd5cf30a393
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-04-19 18:52:49 +0200

    added email validation

--- app/src/main/java/put/inf154030/frog/login_pages/SignUpActivity.kt
+++ app/src/main/java/put/inf154030/frog/login_pages/SignUpActivity.kt
@@ -21,15 +21,18 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -119,6 +122,14 @@ class SignUpActivity : ComponentActivity() {
                             )
                             Spacer(modifier = Modifier.size(16.dp))
                             var password by remember { mutableStateOf("") }
+                            val passwordValid = remember {
+                                derivedStateOf {
+                                    password.length >= 8
+                                            && password.any { it.isUpperCase() }
+                                            && password.any { it.isLowerCase() }
+                                            && password.any { it.isDigit() }
+                                }
+                            }
                             BasicTextField(
                                 value = password,
                                 onValueChange = { newValue -> password = newValue },
@@ -146,8 +157,21 @@ class SignUpActivity : ComponentActivity() {
                                     }
                                 }
                             )
+                            if (password.isNotEmpty() && !passwordValid.value) {
+                                Text(
+                                    text = "Password must have at least 8 characters, 1 uppercase, 1 lowercase, and 1 number.",
+                                    color = Color.Red,
+                                    fontSize = 14.sp,
+                                    modifier = Modifier
+                                        .fillMaxWidth(0.65f),
+                                    textAlign = TextAlign.Center
+                                )
+                            }
                             Spacer(modifier = Modifier.size(16.dp))
                             var passwordConfirmation by remember { mutableStateOf("") }
+                            val passwordsMatch = remember {
+                                derivedStateOf { password == passwordConfirmation }
+                            }
                             BasicTextField(
                                 value = passwordConfirmation,
                                 onValueChange = { newValue -> passwordConfirmation = newValue },
@@ -175,9 +199,21 @@ class SignUpActivity : ComponentActivity() {
                                     }
                                 }
                             )
+                            if (passwordConfirmation.isNotEmpty() && !passwordsMatch.value) {
+                                Text(
+                                    text = "Password do not match.",
+                                    color = Color.Red,
+                                    fontSize = 14.sp,
+                                    modifier = Modifier
+                                        .fillMaxWidth(0.65f),
+                                    textAlign = TextAlign.Center
+                                )
+                            }
                             Spacer(modifier = Modifier.size(64.dp))
                             Button(
-                                onClick = {},
+                                onClick = {
+
+                                },
                                 modifier = Modifier
                                     .fillMaxWidth(0.65f),
                             ) {
