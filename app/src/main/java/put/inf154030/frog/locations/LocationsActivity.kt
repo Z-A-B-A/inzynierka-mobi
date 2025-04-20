edd0005efc45409daf5f66ee215871eedb651ef0
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-04-20 23:54:56 +0200

    added add button in locations view

--- app/src/main/java/put/inf154030/frog/locations/LocationsActivity.kt
+++ app/src/main/java/put/inf154030/frog/locations/LocationsActivity.kt
@@ -3,14 +3,22 @@ package put.inf154030.frog.locations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import put.inf154030.frog.R
 import put.inf154030.frog.fragments.TopNavigationBar
 import put.inf154030.frog.theme.FrogTheme
 
@@ -23,14 +31,31 @@ class LocationsActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        horizontalAlignment = Alignment.CenterHorizontally
+                    Box(
+                        modifier = Modifier.fillMaxSize()
                     ) {
-                        TopNavigationBar(
-                            title = "Hi, XYZ!",
-                            onMenuClick = {  }
-                        )
+                        Column(
+                            modifier = Modifier.fillMaxSize(),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            TopNavigationBar(
+                                title = "Hi, XYZ!",
+                                onMenuClick = {  }
+                            )
+                        }
+                        IconButton(
+                            onClick = {  },
+                            modifier = Modifier
+                                .padding(bottom = 48.dp, end = 32.dp)
+                                .align(Alignment.BottomEnd)
+                        ) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.add_buton),
+                                contentDescription = "Add new location",
+                                tint = MaterialTheme.colorScheme.secondary,
+                                modifier = Modifier.size(48.dp)
+                            )
+                        }
                     }
                 }
             }
@@ -46,14 +71,31 @@ fun LocationsActivityPreview() {
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally
+            Box(
+                modifier = Modifier.fillMaxSize()
             ) {
-                TopNavigationBar(
-                    title = "Hi, XYZ!",
-                    onMenuClick = {  }
-                )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TopNavigationBar(
+                        title = "Hi, XYZ!",
+                        onMenuClick = {  }
+                    )
+                }
+                IconButton(
+                    onClick = {  },
+                    modifier = Modifier
+                        .padding(bottom = 48.dp, end = 32.dp)
+                        .align(Alignment.BottomEnd)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.add_buton),
+                        contentDescription = "Add new location",
+                        tint = MaterialTheme.colorScheme.secondary,
+                        modifier = Modifier.size(48.dp)
+                    )
+                }
             }
         }
     }
