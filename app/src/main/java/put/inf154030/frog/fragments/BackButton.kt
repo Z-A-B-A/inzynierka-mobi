5cf192ad8cdb49a06878e0548c253a93a6d9be31
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-04-25 12:14:20 +0200

    added back button fragment

---
+++ app/src/main/java/put/inf154030/frog/fragments/BackButton.kt
@@ -0,0 +1,41 @@
+package put.inf154030.frog.fragments
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import put.inf154030.frog.theme.FrogTheme
+import put.inf154030.frog.theme.PoppinsFamily
+
+@Composable
+fun BackButton (
+    onClick: () -> Unit
+) {
+    Text(
+        text = "<<< back",
+        color = MaterialTheme.colorScheme.secondary,
+        fontSize = 16.sp,
+        fontFamily = PoppinsFamily,
+        fontWeight = FontWeight.Medium,
+        modifier = Modifier
+            .clickable { onClick() }
+            .padding(8.dp)
+    )
+}
+
+@Preview
+@Composable
+fun BackButtonPreview() {
+    FrogTheme {
+        BackButton(
+            onClick = {  }
+        )
+    }
+}
