a5a47683c834b690ed3dca06fccbcaa6892e13ca
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-05-06 22:58:23 +0200

    added fragment

---
+++ app/src/main/java/put/inf154030/frog/fragments/EditSpeciesRow.kt
@@ -0,0 +1,80 @@
+package put.inf154030.frog.fragments
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import put.inf154030.frog.theme.FrogTheme
+import put.inf154030.frog.theme.PoppinsFamily
+
+@Composable
+fun EditSpeciesRow (
+    speciesName: String,
+    onDeleteClick: () -> Unit
+) {
+    Column (
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row (
+            modifier = Modifier.fillMaxWidth(0.8f),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = speciesName,
+                fontFamily = PoppinsFamily,
+                fontWeight = FontWeight.Medium,
+                fontSize = 20.sp,
+                color = MaterialTheme.colorScheme.secondary
+            )
+            IconButton(
+                onClick = { onDeleteClick() },
+                modifier = Modifier.size(32.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Clear,
+                    contentDescription = "Add parameter",
+                    tint = Color.Red,
+                    modifier = Modifier.fillMaxSize(0.8f)
+                )
+            }
+        }
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .padding(vertical = 8.dp),
+            thickness = 1.dp,
+            color = MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+@Preview
+@Composable
+fun EditSpeciesRowPreview () {
+    FrogTheme {
+        EditSpeciesRow(
+            speciesName = "FROG",
+            onDeleteClick = {  }
+        )
+    }
+}
