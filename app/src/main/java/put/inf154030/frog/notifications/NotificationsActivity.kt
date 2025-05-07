d177932d166f3b31238db8dba1fa263415874daf
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-05-07 22:43:17 +0200

    created last activities

---
+++ app/src/main/java/put/inf154030/frog/notifications/NotificationsActivity.kt
@@ -0,0 +1,15 @@
+package put.inf154030.frog.notifications
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import put.inf154030.frog.R
+
+class NotificationsActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
