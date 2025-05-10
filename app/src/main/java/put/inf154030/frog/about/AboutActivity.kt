60e49ac43a17d3fd706073b166d784d1a24845d9
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-05-10 09:33:04 +0200

    About activity done

--- app/src/main/java/put/inf154030/frog/about/AboutActivity.kt
+++ app/src/main/java/put/inf154030/frog/about/AboutActivity.kt
@@ -2,14 +2,77 @@ package put.inf154030.frog.about
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import put.inf154030.frog.R
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import put.inf154030.frog.fragments.BackButton
+import put.inf154030.frog.fragments.TopHeaderBar
+import put.inf154030.frog.theme.FrogTheme
+import put.inf154030.frog.theme.PoppinsFamily
 
 class AboutActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            FrogTheme {
+                AboutScreen(
+                    onBackClick = { finish() }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun AboutScreen (
+    onBackClick: () -> Unit
+) {
+    Surface (
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column {
+            TopHeaderBar(
+                title = "About"
+            )
+            BackButton { onBackClick() }
+            Box (
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxSize(0.8f),
+                    fontFamily = PoppinsFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.secondary,
+                    textAlign = TextAlign.Center,
+                    text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun AboutActivityPreview () {
+    FrogTheme {
+        AboutScreen(
+            onBackClick = {}
+        )
     }
 }
