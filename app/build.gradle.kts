f82b9a80b5750c8ca40cf075380cbfbeea72826d
Bartosz Korszun <bartoszkorszun@gmail.com>
2025-05-14 20:00:57 +0200

    updated min SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "put.inf154030.frog"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
